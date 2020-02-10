6d2b2511a26216a7c0b3fece1736a8b1c88b82aa
OwenF7 <of752816@scc-mmlab-13.local>
2020-02-10 16:43:28 -0500

    Added Unity Project

---
+++ New Unity Project of Doom/Assets/AgentMover.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class AgentMover : MonoBehaviour
+{
+    [Header("Navigation Components")]
+    public NavMeshAgent agent;
+    public Transform moveTarget;
+    public float defaultMoveSpeed = 5;
+
+    public bool doMoveAtDoubleSpeed;
+    public int myInteger = -56;
+    public float myFloat = 4.5f;
+    public string myString = "wassup world";
+
+
+
+
+
+    void DoubleNumber (float num)
+    {
+        num = num * 2; 
+    }
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(moveTarget.position);
+
+
+
+        if (doMoveAtDoubleSpeed == true)
+        {
+            agent.speed = defaultMoveSpeed * 2; 
+        }
+        else
+        {
+            agent.speed = defaultMoveSpeed; 
+        }
+
+
+
+
+    }
+}
